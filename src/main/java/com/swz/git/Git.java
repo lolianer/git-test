e92caed2f3055bd82895c6333b094bcaeae41ba2
shen_wzhong <1172595124@qq.com>
2021-07-19 09:26:21 +0800

    first commit

---
+++ src/main/java/com/swz/git/Git.java
@@ -0,0 +1,11 @@
+package com.swz.git;
+
+/**
+ * @author shen_wzhong
+ * @create 2021-07-19 9:21
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
